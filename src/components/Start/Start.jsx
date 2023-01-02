4a5f81a6e26abc19978a3ec5e4fb0088392cc171
bsenk <bsenk77@gmail.com>
2023-01-02 14:00:46 +0200

    make category page

--- src/components/Start/Start.jsx
+++ src/components/Start/Start.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 import SearthBar from "../SearthBar/SearthBar";
 import s from "./Start.module.css";
 
-
 export const Start = (props) => {
-  
+  const categories = useSelector((store) => store.main.categories);
   return (
     <main className={s.main}>
-      <SearthBar/>
+      <SearthBar />
+      <div className={s.catTitle}>Категорії</div>
+      <div className={s.categories}>
+        {categories.map((item) => {
+          return (
+            <div className={s.catItem} key={item.id}>
+              <img alt="category item" src={item.img} />
+              <div className={s.itemtitle}>{item.title}</div>
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 };
--- src/store/Main/MainReducer.js
+++ src/store/Main/MainReducer.js
@@ -3,6 +3,29 @@ import { CLEAR_SORT_TYPE, SEARTCH_CHANGE, SET_SORT_TYPE } from "./MainActions";
 const initalstate = {
   sortType: false,
   searchText: "",
+  categories: [
+    { id: 1, title: "Подарункові сертифікати", img: "../image/Category/2.png" },
+    { id: 2, title: "Arduino контроллери", img: "../image/Category/1.png" },
+    { id: 3, title: "Мікрокомпютери", img: "../image/Category/3.png" },
+    { id: 4, title: "Raspberry PI", img: "../image/Category/4.png" },
+    { id: 5, title: "Засоби розробки, програматори", img: "../image/Category/5.png" },
+    { id: 6, title: "Карти памяті, SD, Флешки", img: "../image/Category/6.png" },
+    { id: 7, title: "Набори (DIY Kits) конструктори", img: "../image/Category/7.png" },
+    { id: 8, title: "Подарункові сертифікати", img: "../image/Category/2.png" },
+    { id: 9, title: "Arduino контроллери", img: "../image/Category/1.png" },
+    { id: 10, title: "Мікрокомпютери", img: "../image/Category/3.png" },
+    { id: 11, title: "Raspberry PI", img: "../image/Category/4.png" },
+    { id: 12, title: "Засоби розробки, програматори", img: "../image/Category/5.png" },
+    { id: 13, title: "Карти памяті, SD, Флешки", img: "../image/Category/6.png" },
+    { id: 14, title: "Набори (DIY Kits) конструктори", img: "../image/Category/7.png" },
+    { id: 15, title: "Подарункові сертифікати", img: "../image/Category/2.png" },
+    { id: 16, title: "Arduino контроллери", img: "../image/Category/1.png" },
+    { id: 17, title: "Мікрокомпютери", img: "../image/Category/3.png" },
+    { id: 18, title: "Raspberry PI", img: "../image/Category/4.png" },
+    { id: 19, title: "Засоби розробки, програматори", img: "../image/Category/5.png" },
+    { id: 20, title: "Карти памяті, SD, Флешки", img: "../image/Category/6.png" },
+    { id: 21, title: "Набори (DIY Kits) конструктори", img: "../image/Category/7.png" },
+  ],
 };
 
 export function MainReducer(store = initalstate, action) {
